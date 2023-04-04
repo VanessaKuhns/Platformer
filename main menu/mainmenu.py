63753e2de17db31c6d6a45ae316a71306b7ab0a5
VanessaKuhns <vkkuhn1747@schreiner.edu>
2023-04-04 11:09:46 -0500

    added 2 new buttons

--- main menu/mainmenu.py
+++ main menu/mainmenu.py
@@ -1,11 +1,20 @@
 #setup
 import tkinter
+import PyQt5
+from PyQt5 import QtGui
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import (QMenuBar, QStatusBar, QApplication, QMainWindow, QPushButton, QToolTip, QMessageBox, QLabel, QVBoxLayout, QWidget)
+import site
+site.getsitepackages()
+
+
 import pygame, sys, random
 from pygame.locals import *
 from tkinter import *
 import os
 os.chdir('main menu')
-
 # button4 = pygame.image.load('buttongradient.png')
 # button4flip = pygame.image.load('buttongradient.png')
 # button4flip = pygame.transform.flip(button4flip, True, False)
@@ -14,6 +23,12 @@ os.chdir('main menu')
 # rect4flip = button4flip.get_rect()
 # rect4flip = rect4flip.move((x/4, y-400))
 # lvlselect = fontobj.render('Select Character', True, (0,0,0), None)
+
+from random import randint
+
+
+
+
 
 pygame.init()
 x = 1000
@@ -24,11 +39,12 @@ window = pygame.display.set_mode((x,y))
 #background setup
 backg = pygame.image.load('gradient.png')
 backg = pygame.transform.scale(backg, (x, y))
-# Sorry if the x and y values get confusing :(
-# I tend to adjust them in really weird ways
+
+
 
 #font setup
 fontobj = pygame.font.SysFont('impact', 32)
+fontobj2 = pygame.font.SysFont('impact', 25)
 
 #button1 (New Game) setup
 button1 = pygame.image.load('buttongradient.png')
@@ -70,10 +86,33 @@ rect4flip = button4flip.get_rect()
 rect4flip = rect4flip.move((x/4, y-190))
 loadgame = fontobj.render('Load Game', True, (0,0,0), None)
 
+#settings
+button5 = pygame.image.load('cog.png')
+button5flip = pygame.image.load('coginvert.png')
+button5flip= pygame.transform.scale(button5flip, (80,80))
+button5= pygame.transform.scale(button5, (80,80))
+#button5flip = pygame.transform.flip(button4flip, True, False)
+rect5 = button5.get_rect()
+rect5 = rect5.move((x-100, y-100))
+rect5flip = button5flip.get_rect()
+rect5flip = rect5flip.move((x-100, y-100))
+
+#credits
+button6 = pygame.image.load('buttongradient.png')
+button6flip = pygame.image.load('buttongradient.png')
+button6flip = pygame.transform.flip(button6flip, True, False)
+button6flip = pygame.transform.scale(button6flip, (200,60))
+button6 = pygame.transform.scale(button6, (200,60))
+rect6 = button6.get_rect()
+rect6 = rect6.move((x-590, y-80))
+rect6flip = button6flip.get_rect()
+rect6flip = rect6flip.move((x-590, y-80))
+creditsshow = fontobj.render('Credits', True, (0,0,0), None)
+
+#loadgame = fontobj.render('Load Game', True, (0,0,0), None)
 
 
 while True:
-
     window.blit(backg, (0,0))
     window.blit(button1, rect1)
     window.blit(button1flip, rect1)
@@ -87,7 +126,11 @@ while True:
     window.blit(button4, rect4)
     window.blit(button4flip, rect4)
     window.blit(loadgame, (x-560, y-160))
-
+    window.blit(button5, rect5)
+    window.blit(button5flip, rect5)
+    window.blit(button6, rect6)
+    window.blit(button6flip, rect6)
+    window.blit(creditsshow, (x-540, y-70))
     #get mouse position in separate x and y values
     mouseX, mouseY = pygame.mouse.get_pos()
 
@@ -122,6 +165,18 @@ while True:
     else:
         button4.set_alpha(255)
         button4flip.set_alpha(0)
+    if(900 <= mouseX <= 980 and 500 <= mouseY <= 580):
+        button5flip.set_alpha(255)
+        button5.set_alpha(0)
+    else:
+        button5.set_alpha(255)
+        button5flip.set_alpha(0)
+    if(400 <= mouseX <= 610 and 520 <= mouseY <= 580):
+        button6flip.set_alpha(255)
+        button6.set_alpha(0)
+    else:
+        button6.set_alpha(255)
+        button6flip.set_alpha(0)
     pygame.display.flip()
 
 
